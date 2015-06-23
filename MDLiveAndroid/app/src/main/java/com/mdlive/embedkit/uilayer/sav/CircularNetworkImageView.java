60f579f703b38a9e189cd4e31cc2d0102a7daa65
Sudha <sudha.subramanian@photoninfotech.net>
2015-06-24 01:09:59 +0530

    CircularImageView and styles commit.

---
+++ MDLiveAndroid/app/src/main/java/com/mdlive/embedkit/uilayer/sav/CircularNetworkImageView.java
@@ -0,0 +1,64 @@
+package com.mdlive.embedkit.uilayer.sav;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
+import android.util.AttributeSet;
+
+import com.android.volley.toolbox.NetworkImageView;
+
+/**
+ * Created by dhiman_da on 6/23/2015.
+ */
+public class CircularNetworkImageView extends NetworkImageView {
+    public CircularNetworkImageView(Context context) {
+        super(context);
+    }
+
+    public CircularNetworkImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CircularNetworkImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    public void setImageBitmap(Bitmap bm) {
+        if (bm == null) {
+            return;
+        }
+
+        setImageDrawable(new BitmapDrawable(getContext().getResources(), createCircularBitmap(bm)));
+    }
+
+    private Bitmap createCircularBitmap(final Bitmap bitmap) {
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
+                bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+        int width = bitmap.getWidth();
+        if(bitmap.getWidth()>bitmap.getHeight())
+            width = bitmap.getHeight();
+        final int color = 0xff424242;
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, width, width);
+        final RectF rectF = new RectF(rect);
+        final float roundPx = width / 2;
+
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
+
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+
+        return output;
+    }
+}
