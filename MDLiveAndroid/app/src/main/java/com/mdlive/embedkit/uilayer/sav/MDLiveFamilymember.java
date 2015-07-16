a5dd641f5b254af5114cbb0b0f2b6624a350d448
unknown <prabhu_a@DS_PRABHU_A.PHOTON>
2015-07-16 14:15:10 +0530

    Jira#PMA-433 fix.

--- MDLiveAndroid/app/src/main/java/com/mdlive/embedkit/uilayer/sav/MDLiveFamilymember.java
+++ MDLiveAndroid/app/src/main/java/com/mdlive/embedkit/uilayer/sav/MDLiveFamilymember.java
@@ -118,6 +118,24 @@ public class MDLiveFamilymember extends MDLiveBaseActivity {
             }
         });
 
+        lastNameEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                ValidateModuleFields();
+            }
+        });
+
+
         //Add child btn
         addChildBtn.setOnClickListener(new View.OnClickListener() {
             @Override
