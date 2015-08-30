f40372aabd214f182e8def350af9cf11a258c443
Venkataraman <venkataraman_r@DS_VENKATARAMAN_R.PHOTON>
2015-08-30 22:37:37 +0530

    Waiting Room changes.

---
+++ MDLiveAndroid/app/src/main/java/com/mdlive/embedkit/uilayer/WaitingRoom/WaitingRoomViewPager.java
@@ -0,0 +1,42 @@
+package com.mdlive.embedkit.uilayer.WaitingRoom;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.Scroller;
+
+import java.lang.reflect.Field;
+
+/**
+ * Created by venkataraman_r on 8/28/2015.
+ */
+public class WaitingRoomViewPager extends ViewPager {
+
+    public WaitingRoomViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setMyScroller();
+    }
+
+    private void setMyScroller() {
+        try {
+            Class<?> viewpager = ViewPager.class;
+            Field scroller = viewpager.getDeclaredField("mScroller");
+            scroller.setAccessible(true);
+            scroller.set(this, new MyScroller(getContext()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public class MyScroller extends Scroller {
+        public MyScroller(Context context) {
+            super(context, new DecelerateInterpolator());
+        }
+
+        @Override
+        public void startScroll(int startX, int startY, int dx, int dy, int duration) {
+            super.startScroll(startX, startY, dx, dy, 1000 /*1 secs*/);
+        }
+    }
+}
