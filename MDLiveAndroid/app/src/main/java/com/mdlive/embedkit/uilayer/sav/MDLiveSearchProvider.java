e62ad3c6487ec82572ad7c088af21241393bf40f
Balaji Ganesh <balaji.muthuswamy@photoninfotech.net>
2015-09-23 22:44:17 +0530

    Choose provider string updated.

--- MDLiveAndroid/app/src/main/java/com/mdlive/embedkit/uilayer/sav/MDLiveSearchProvider.java
+++ MDLiveAndroid/app/src/main/java/com/mdlive/embedkit/uilayer/sav/MDLiveSearchProvider.java
@@ -102,7 +102,7 @@ public class MDLiveSearchProvider extends MDLiveBaseActivity {
 
         ((ImageView) findViewById(R.id.backImg)).setImageResource(R.drawable.back_arrow_hdpi);
         ((ImageView) findViewById(R.id.txtApply)).setImageResource(R.drawable.top_tick_icon);
-        ((TextView) findViewById(R.id.headerTxt)).setText(getString(R.string.mdl_choose_provicer));
+        ((TextView) findViewById(R.id.headerTxt)).setText(getString(R.string.mdl_choose_provider_caps));
 
 
         initialiseData();
