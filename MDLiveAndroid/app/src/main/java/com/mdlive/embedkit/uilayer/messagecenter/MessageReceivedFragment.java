78f3001b67e28bad58fe818f8a9db0eb11bb7b98
Srinivasan <srinivasan.karunagaran@photoninfotech.net>
2015-10-20 15:44:18 +0530

    Message center notification issues fixed.

--- MDLiveAndroid/app/src/main/java/com/mdlive/embedkit/uilayer/messagecenter/MessageReceivedFragment.java
+++ MDLiveAndroid/app/src/main/java/com/mdlive/embedkit/uilayer/messagecenter/MessageReceivedFragment.java
@@ -71,7 +71,8 @@ public class MessageReceivedFragment extends MDLiveBaseFragment {
         mBlankLayout = view.findViewById(R.id.blank_layout);
         mBlankLayout.setVisibility(View.GONE);
         if (mListView != null) {
-            mMessageReceivedAdapter = new MessageReceivedAdapter(view.getContext(), R.layout.adapter_message_received, android.R.id.text1);
+            mMessageReceivedAdapter = new MessageReceivedAdapter(view.getContext(),
+                    R.layout.adapter_message_received, android.R.id.text1);
             mListView.setAdapter(mMessageReceivedAdapter);
             mListView.setOnScrollListener(new InfiniteScrollListener(0) {
                 @Override
@@ -82,12 +83,11 @@ public class MessageReceivedFragment extends MDLiveBaseFragment {
             mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                 @Override
                 public void onItemClick(AdapterView<?> adapterView, View view, final int i, long l) {
-                    mMessageReceivedAdapter.getItem(i).readStatus = true;
-                    mMessageReceivedAdapter.getView(i, view, adapterView).invalidate();
-
                     if (getActivity() != null && getActivity() instanceof MessageCenterActivity) {
                         ((MessageCenterActivity) getActivity()).onReceivedMessageClicked(mMessageReceivedAdapter.getItem(i));
                     }
+                    mMessageReceivedAdapter.getItem(i).readStatus = true;
+                    mMessageReceivedAdapter.getView(i, view, adapterView).invalidate();
                 }
             });
         }
@@ -164,7 +164,6 @@ public class MessageReceivedFragment extends MDLiveBaseFragment {
             @Override
             public void onResponse(JSONObject response) {
                 hideProgressDialog();
-
                 handleSucess(response);
             }
         };
