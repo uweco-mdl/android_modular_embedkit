1ac4c0ccc14f3e11774f2a2901f4ba4638701a49
Oke Uwechue <ouwechue@mdlive.com>
2016-03-01 16:53:12 -0500

    UnifiedMiddleware Dec 2nd “MDLIVE-Enhancement” Commit#3c3ffda439ab680ff6b2de2147247477d1817f64 (Code updates for efficiency) merged

--- MDLiveAndroid/app/src/main/java/com/mdlive/unifiedmiddleware/services/provider/ProviderDetailServices.java
+++ MDLiveAndroid/app/src/main/java/com/mdlive/unifiedmiddleware/services/provider/ProviderDetailServices.java
@@ -17,8 +17,12 @@ public class ProviderDetailServices extends BaseServicesPlugin {
             super(context,pDialog);
 
     }
-    public void getProviderDetails(String locatedIn, String appointementDate, String appointmentType,
-                                   String DoctorId, NetworkSuccessListener<JSONObject> responseListener , NetworkErrorListener errorListener){
+    public void getProviderDetails(String locatedIn,
+                                   String appointementDate,
+                                   String appointmentType,
+                                   String DoctorId,
+                                   NetworkSuccessListener<JSONObject> responseListener,
+                                   NetworkErrorListener errorListener){
         try {
             Log.e("appmtDate",appointementDate);
             jsonObjectGetRequest(AppSpecificConfig.BASE_URL + AppSpecificConfig.URL_PROVIDER_DETAILS + DoctorId + "?appointment_date=" + appointementDate + "&appointment_type=" + appointmentType + "&located_in=" + locatedIn,
@@ -32,13 +36,32 @@ public class ProviderDetailServices extends BaseServicesPlugin {
         }
     }
 
-    public void getProviderDetails(String DoctorId, NetworkSuccessListener<JSONObject> responseListener , NetworkErrorListener errorListener){
+    public void getProviderDetails(String DoctorId,
+                                   NetworkSuccessListener<JSONObject> responseListener,
+                                   NetworkErrorListener errorListener){
         try {
             jsonObjectGetRequest(AppSpecificConfig.BASE_URL + AppSpecificConfig.URL_PROVIDER_DETAILS + DoctorId,
                                     null,
                                     responseListener,
                                     errorListener,
                                     MDLiveConfig.IS_SSO);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void getProviderDetails(String appointmentDate,
+                                   String appointmentType,
+                                   String DoctorId,
+                                   NetworkSuccessListener<JSONObject> responseListener,
+                                   NetworkErrorListener errorListener){
+        try {
+            Log.e("appmtDate",appointmentDate);
+            jsonObjectGetRequest(AppSpecificConfig.BASE_URL + AppSpecificConfig.URL_PROVIDER_DETAILS + DoctorId + "?appointment_date=" + appointmentDate + "&appointment_type=" + appointmentType,
+                    null,
+                    responseListener,
+                    errorListener,
+                    MDLiveConfig.IS_SSO);
         }catch(Exception e){
             e.printStackTrace();
         }
