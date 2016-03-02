3e44e00b838c8c136715140d76de366551ebbf9b
Oke Uwechue <ouwechue@mdlive.com>
2016-03-02 10:07:03 -0500

    UnifiedMiddleware Dec 8th “MDLIVE_Enhancement” Commit#2b7f6119a4315499dcf53a99cb6c9363f0d38ad1 (Fixed MM-932 Crash issue on clicking scan button) merged

--- MDLiveAndroid/app/src/main/java/com/mdlive/unifiedmiddleware/plugins/CardIOPlugin.java
+++ MDLiveAndroid/app/src/main/java/com/mdlive/unifiedmiddleware/plugins/CardIOPlugin.java
@@ -3,6 +3,8 @@ package com.mdlive.unifiedmiddleware.plugins;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
+
 import com.mdlive.unifiedmiddleware.commonclasses.constants.IdConstants;
 
 import io.card.payment.CardIOActivity;
@@ -15,17 +17,22 @@ public class CardIOPlugin {
 
     /**
      *
-     * This function scans the credit card number by calling teh CardIOActivity and
+     * This function scans the credit card number by calling the CardIOActivity and
      * the results are passed to activity result of the context.
      *
      * @param context
      */
     public static void scanCard(Context context){
-        Intent scanIntent = new Intent(context, CardIOActivity.class);
-        scanIntent.putExtra(CardIOActivity.EXTRA_REQUIRE_EXPIRY, false);
-        scanIntent.putExtra(CardIOActivity.EXTRA_REQUIRE_CVV, false);
-        scanIntent.putExtra(CardIOActivity.EXTRA_REQUIRE_POSTAL_CODE, false);
-        scanIntent.putExtra(CardIOActivity.EXTRA_SUPPRESS_MANUAL_ENTRY, true);
-        ((Activity)context).startActivityForResult(scanIntent, IdConstants.CREDITCARD_SCAN);
+        try {
+            Intent scanIntent = new Intent(context, CardIOActivity.class);
+            scanIntent.putExtra(CardIOActivity.EXTRA_REQUIRE_EXPIRY, false);
+            scanIntent.putExtra(CardIOActivity.EXTRA_REQUIRE_CVV, false);
+            scanIntent.putExtra(CardIOActivity.EXTRA_REQUIRE_POSTAL_CODE, false);
+            scanIntent.putExtra(CardIOActivity.EXTRA_SUPPRESS_MANUAL_ENTRY, true);
+            ((Activity) context).startActivityForResult(scanIntent, IdConstants.CREDITCARD_SCAN);
+        }catch (NoClassDefFoundError e) {
+            Log.e("CardIOPlugin:scanCard()", "Runtime crash: "+e.getMessage());
+
+        }
     }
 }
